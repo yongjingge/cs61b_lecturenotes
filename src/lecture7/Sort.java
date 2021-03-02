cde95573aa4cda2e437f4eea6c59a22eea7d919b
yongjing <near711@outlook.com>
2021-03-02 11:51:45 +0800

    lecture7 junit test on SelectionSort

--- src/lecture7/Sort.java
+++ src/lecture7/Sort.java
@@ -1,2 +1,43 @@
-package lecture7;public class Sort {
+package lecture7;
+
+/* a test-driven development example of SelectionSort */
+public class Sort {
+
+    /* sort strings destructively */
+    public static void sort(String[] s) {
+        /* find the smallest item */
+        /* move it to the front */
+        /* selection sort the rest using recursion */
+        if (s == null || s.length <= 1) {
+            return;
+        }
+        sort(s, 0);
+    }
+
+    public static int findSmallest(String[] s, int start) {
+        int smallestIndex = start;
+        for (int i = start; i < s.length; i += 1) {
+            int cmp = s[i].compareTo(s[smallestIndex]);
+            if (cmp < 0) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static void swap(String[] s, int i, int j) {
+        String tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+
+    /* a helper method to conduct recursion */
+    public static void sort(String[] s, int start) {
+        if (start == s.length) {
+            return;
+        }
+        int smallestIndex = findSmallest(s, start);
+        swap(s, start, smallestIndex);
+        sort(s, start + 1);
+    }
 }
--- src/lecture7/TestSort.java
+++ src/lecture7/TestSort.java
@@ -1,2 +1,30 @@
-package lecture7;public class TestSort {
+package lecture7;
+
+/* test the Sort class */
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestSort {
+
+    @Test
+    public void testFindSmallestIndex() {
+        String[] s = {"i", "have", "an", "egg"};
+        int smallestIndex = Sort.findSmallest(s, 0);
+        assertEquals(2, smallestIndex);
+    }
+
+    @Test
+    public void testSwap() {
+        String[] s = {"i", "have", "an", "egg"};
+        String[] afterSwap = {"an", "have", "i", "egg"};
+        Sort.swap(s, 0, 2);
+        assertArrayEquals(afterSwap, s);
+    }
+
+    @Test
+    public void testSort() {
+        String[] s = {"i", "have", "an", "egg"};
+        String[] expected = {"an", "egg", "have", "i"};
+        Sort.sort(s);
+        assertArrayEquals(expected, s);
+    }
 }
