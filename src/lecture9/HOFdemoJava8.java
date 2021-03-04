8c6e1d5b030f614e44a625e0ab914012913e8b63
yongjing <near711@outlook.com>
2021-03-04 12:50:52 +0800

    lecture9 Higher Order Function demo in Java8

--- src/lecture9/HOFdemoJava8.java
+++ src/lecture9/HOFdemoJava8.java
@@ -1,2 +1,20 @@
-package lecture9;public class HOFdemoJava8 {
+package lecture9;
+
+import java.util.function.Function;
+
+/* Java8 version of HOFdemo */
+public class HOFdemoJava8 {
+
+    public static int tenX(int x) {
+        return 10 * x;
+    }
+
+    public static int do_twice(Function<Integer, Integer> function, int x) {
+        return function.apply(function.apply(x));
+    }
+
+    public static void main(String[] args) {
+        int res = do_twice(HOFdemoJava8::tenX, 2);
+        System.out.println(res);
+    }
 }
