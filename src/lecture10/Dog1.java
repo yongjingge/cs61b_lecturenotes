035fb288df37d0873998ae6f0af6bb28f53a965e
yongjing <near711@outlook.com>
2021-03-04 21:28:43 +0800

    lecture10 extends Comparable

---
+++ src/lecture10/Dog1.java
@@ -0,0 +1,26 @@
+package lecture10;
+
+/* implements a more commonly used built-in Comparable interface */
+public class Dog1 implements Comparable<Dog1> {
+
+    private String name;
+    private int size;
+
+    public Dog1(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    @Override
+    public int compareTo(Dog1 other) {
+        return this.size - other.size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
--- src/lecture10/Maximizer.java
+++ src/lecture10/Maximizer.java
@@ -1,5 +1,8 @@
 package lecture10;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /* provide a max method */
 public class Maximizer {
 
@@ -14,10 +17,28 @@ public class Maximizer {
         return os[maxIndex];
     }
 
+    public static Comparable max(Comparable[] os) {
+        int maxIndex = 0;
+        for (int i = 1; i < os.length; i += 1) {
+            int cmp = os[i].compareTo(os[maxIndex]);
+            if (cmp > 0) {
+                maxIndex = i;
+            }
+        }
+        return os[maxIndex];
+    }
+
     public static void main(String[] args) {
-        Dog[] dogs = {new Dog("a", 1), new Dog("b", 3), new Dog("c", 22)};
-        Dog max = (Dog) max(dogs);
+//        Dog[] dogs = {new Dog("a", 1), new Dog("b", 3), new Dog("c", 22)};
+//        Dog max = (Dog) max(dogs);
+//        System.out.println(max.getName());
+
+        Dog1[] dogs = {new Dog1("a", 1), new Dog1("b", 3), new Dog1("c", 22)};
+        Dog1 max = (Dog1) max(dogs);
         System.out.println(max.getName());
+
+        Dog1 largest = Collections.max(Arrays.asList(dogs));
+        System.out.println(largest.getName());
     }
 
 }
