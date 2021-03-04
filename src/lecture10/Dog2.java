721ac67d842882ceaa82e6668f893712cccbdf9b
yongjing <near711@outlook.com>
2021-03-04 21:55:13 +0800

    lecture10 Nested Class implements Comparator

--- src/lecture10/Dog2.java
+++ src/lecture10/Dog2.java
@@ -1,12 +1,9 @@
 package lecture10;
 
-import lombok.Getter;
-
 import java.util.Comparator;
 
 /* have a nested class 'NameComparator' that implements Comparator interface
 * and overrides the compare method */
-@Getter
 public class Dog2 {
 
     private String name;
@@ -32,6 +29,16 @@ public class Dog2 {
         return new NameComparator();
     }
 
+    public static class SizeComparator implements Comparator<Dog2> {
+        public int compare(Dog2 d1, Dog2 d2) {
+            return d1.size - d2.size;
+        }
+    }
+
+    public static Comparator<Dog2> getSizeComparator() {
+        return new SizeComparator();
+    }
+
     public static void main(String[] args) {
         Dog2 d1 = new Dog2("a", 11);
         Dog2 d2 = new Dog2("b", 12);
@@ -40,6 +47,13 @@ public class Dog2 {
             System.out.println(d1.name);
         } else {
             System.out.println(d2.name);
+        }
+
+        Comparator<Dog2> sc = getSizeComparator();
+        if (sc.compare(d1, d2) > 0) {
+            System.out.println(d1.size);
+        } else {
+            System.out.println(d2.size);
         }
     }
 }
