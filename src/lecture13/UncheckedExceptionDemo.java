912fb103d3d376a87a16935575fdff0a344f2a10
yongjing <near711@outlook.com>
2021-03-07 13:47:06 +0800

    lecture14 Exceptions Unchecked vs Checked

---
+++ src/lecture13/UncheckedExceptionDemo.java
@@ -0,0 +1,21 @@
+package lecture13;
+
+import java.io.IOException;
+import java.util.Date;
+
+/* Unchecked Exception Demo
+* code throws exceptions and the compiler will be fine with that. */
+public class UncheckedExceptionDemo {
+    public static void main(String[] args) {
+        String today = "Thursday";
+        /* the code runs without crashing, Process finished with exit code 1 */
+        if (today == "Thursday") {
+            throw new RuntimeException("this is an Unchecked exception!");
+        }
+
+        /* code crashes! unreported exception, must be caught or declared to be thrown */
+        if (today == "Thursday") {
+            throw new IOException("this is a checked exception!");
+        }
+    }
+}
